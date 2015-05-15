5d7c564171b97d72aa04a1bad1811d16c7b544dd
Andriy Bondar <abondar@mirantis.com>
2015-05-15 14:02:08 +0300

    Added removing 'stable' symlink before repointing

--- change_symlink.sh
+++ change_symlink.sh
@@ -5,8 +5,9 @@ if [ "$change_symlink" = "true" ]; then
     echo "=== Change symlink"
     snapshot=`ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null root@${STORAGE_IP} -p ${STORAGE_PORT} ls -l /store/fuel_ref/rc | awk '{print $11}'`
 
+    ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null root@${STORAGE_IP} -p ${STORAGE_PORT} "rm -f /store/fuel_ref/stable"
     ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null root@${STORAGE_IP} -p ${STORAGE_PORT} "ln -s $snapshot /store/fuel_ref/stable"
     ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null root@${STORAGE_IP} -p ${STORAGE_PORT} "ls -lh /store/fuel_ref/stable"
 else
-    echo "=== Changin symlink skipped"
+    echo "=== Changing symlink skipped"
 fi
