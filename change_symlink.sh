79c6b1440da54ee6cf03ec6447054264d764fa9f
Andriy Bondar <abondar@mirantis.com>
2015-05-15 13:51:07 +0300

    Added script to re-point symlink

---
+++ change_symlink.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+
+if [ "$change_symlink" = "true" ]; then
+    echo "=== Change symlink"
+    snapshot=`ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null root@${STORAGE_IP} -p ${STORAGE_PORT} ls -l /store/fuel_ref/rc | awk '{print $11}'`
+
+    ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null root@${STORAGE_IP} -p ${STORAGE_PORT} "ln -s $snapshot /store/fuel_ref/stable"
+    ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null root@${STORAGE_IP} -p ${STORAGE_PORT} "ls -lh /store/fuel_ref/stable"
+else
+    echo "=== Changin symlink skipped"
+fi
